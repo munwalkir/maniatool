1517fc3a5cfe70112f2ed48faf1b9cb1f89fc998
munwalkir <munwalkir@gmail.com>
2025-08-03 01:36:45 +0300

    Add fonts and update tailwind config

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="shadow-black shadow-1x/30 relative w-full max-w-sm p-6 bg-container rounded-3xl">
+      <h1 class="absolute left-1/2 -translate-x-1/2 text-pink-400 text-6xl" style="top: -60%">
+        Login
+      </h1>
+      <h4 class="text-white text-2xl font-bold mb-4">Username:</h4>
+      <input type="text" placeholder="" class="<!--burayı daha yapmadım.tamam.-->" />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
